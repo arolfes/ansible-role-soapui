9d6d3b16aa214689631374ef9e22745e6f5b8c3f
arolfes <arolfes@users.noreply.github.com>
2022-04-17 12:38:55 +0200

    Closes #2: possibility to define java path for all sh files

---
+++ molecule/ubuntu-max-soapui-max-java-home/tests/test_role.py
@@ -0,0 +1,52 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize('version_dir_pattern', [
+    '5.7.0$'
+])
+def test_soapui_installed(host, version_dir_pattern):
+
+    soapui_home = host.check_output('find %s | grep --color=never -E %s',
+                                    '/opt/soapui/',
+                                    version_dir_pattern)
+
+    soapui_sh = host.file(soapui_home + '/bin/soapui.sh')
+
+    assert soapui_sh.exists
+    assert soapui_sh.is_file
+    assert soapui_sh.user == 'root'
+    assert soapui_sh.group == 'root'
+    assert oct(soapui_sh.mode) == '0o755'
+
+
+def test_soapui(host):
+    assert host.run('which soapui.sh').rc == 0
+
+
+def test_soapui_desktop_file(host):
+    desk_file = host.file('/usr/share/applications/SoapUI.desktop')
+
+    assert desk_file.exists
+    assert desk_file.is_file
+    assert desk_file.user == 'root'
+    assert desk_file.group == 'root'
+    assert oct(desk_file.mode) == '0o644'
+
+
+@pytest.mark.parametrize('fact_group_name', [
+    'soapui'
+])
+def test_facts_installed(host, fact_group_name):
+    fact_file = host.file('/etc/ansible/facts.d/' + fact_group_name + '.fact')
+
+    assert fact_file.exists
+    assert fact_file.is_file
+    assert fact_file.user == 'root'
+    assert fact_file.group == 'root'
+    assert oct(fact_file.mode) == '0o644'
